604d64761dc44fab055c812de187b721cdea0cb2
parvathy <parvathy@2498623e-5ac1-4a62-989a-52cda5f0dd9e>
2009-09-03 10:30:39 +0000

    removing validate_associated for name & address

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,15 +3,20 @@ class User < ActiveRecord::Base
   composed_of :address
   validates_presence_of :email, :person_type
   validates_uniqueness_of :email
-  validates_associated :name, :address
 #  validates_confirmation_of :password
 #  validate :password_non_blank
 #  attr_accessor :password_confirmation
+  validate :valid_name
   
   private
-  def password_non_blank
-    errors.add(:password, "Missing password") if hashed_password.blank?
+  def valid_name
+     errors.add(:name, "Missing name") if name.nil?
+     name.is_valid?(errors)
   end
+  
+#  def password_non_blank
+#    errors.add(:password, "Missing password") if hashed_password.blank?
+#  end
   
   
   # validates_format_of :email, :with => %r{(.+)@(.+)(\.(.{1,3})){1,2}$}i, :message => "Invalid email id"
@@ -23,7 +28,7 @@ class User < ActiveRecord::Base
 end
 
 class Address
-  include ActiveRecord::Validations
+ # include ActiveRecord::Validations
   attr_reader :address_line1,  :address_line2,  :city,  :state,  :country,  :pincode,  :phone_landline,  :phone_mobile
   def initialize(address_line1,  address_line2,  city,  state,  country,  pincode,  phone_landline,  phone_mobile)
     @address_line1=address_line1
@@ -35,17 +40,21 @@ class Address
     @phone_landline=phone_landline  
     @phone_mobile =  phone_mobile
   end
-  validates_presence_of :address_line1,   :city,    :state,    :country
+ # validates_presence_of :address_line1,   :city,    :state,    :country
   #TODO validates_format_of pincode,landline,mobile  
 end
 
 class Name
-  include ActiveRecord::Validations
+ # include ActiveRecord::Validations
   attr_reader :first_name, :middle_name, :last_name
   def initialize(first_name, middle_name, last_name)
     @first_name=first_name
     @middle_name=middle_name
     @last_name=last_name
   end
-  validates_presence_of :first_name, :last_name
+  def is_valid?(errors)
+    errors.add("first name should not be blank") if first_name.blank? 
+    errors.add("last name should not be blank") if last_name.blank?
+  end
+ # validates_presence_of :first_name, :last_name
 end
