99525ff1b4f8884ae05d54ded9d118f1dcc9165c
parvathy <parvathy@2498623e-5ac1-4a62-989a-52cda5f0dd9e>
2009-09-03 17:20:11 +0000

    User Model with validations

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -2,7 +2,13 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "invalid with null names" do
+    name=Name.new('f','m','l')
+    user=User.new(:email => "user@schoolarly.com")
+    user.name=name
+    assert !user.valid?
+    #assert user.errors.invalid?(:name)
+    #assert user.errors.valid?(:email)
   end
 end
+
