6959805684061f220ff3be4147822e79e7d07db0
shenuja <shenuja@2498623e-5ac1-4a62-989a-52cda5f0dd9e>
2009-09-08 07:09:21 +0000

    adding current_students

--- app/models/klass.rb
+++ app/models/klass.rb
@@ -3,6 +3,9 @@ class Klass < ActiveRecord::Base
   belongs_to :class_teacher, :class_name => 'Teacher', :foreign_key => 'teacher_id'
   has_many :enrollments, :class_name =>'StudentEnrollment'  
   has_many :students, :through => :enrollments
-  has_many :current_students,:through => :enrollments, :source=>:student
   has_and_belongs_to_many :subjects
+  
+  def current_students
+    self.enrollments.select{|e| e.current_student}
+  end
 end
