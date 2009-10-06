796b105fd95c69ad980afec2eded5a4ac234cf3d
parvathy <parvathy@2498623e-5ac1-4a62-989a-52cda5f0dd9e>
2009-10-06 19:47:47 +0000

    Transactions added for Profile Create & Profile Update.

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -20,14 +20,15 @@ class SchoolsController < ApplicationController
   def show
     @active_tab = :Home
     @school=School.find(params[:id])
-  	set_active_user(@school.user)
+    set_active_user(@school.user)
+    
     respond_to do |format|
       format.html # show.html.erb
       format.xml  { render :xml => @school }
     end
   end
   
-    
+  
   # GET /schools/new
   # GET /schools/new.xml
   def new
@@ -66,14 +67,13 @@ class SchoolsController < ApplicationController
   # PUT /schools/1
   # PUT /schools/1.xml
   def update
-    @active_tab = :Profile
+    #@active_tab = :Profile
     @school = School.find(params[:id])
-    @user=@school.user#User.find_by_person_id(params[:id])
-    @user_profile=@user.user_profile
+    
     respond_to do |format|
-      if @school.update_attributes(params[:school]) && @user.update_attributes(params[:user]) && @user_profile.update_attributes(params[:user_profile])
+      if @school.update_attributes(params[:school]) 
         flash[:notice] = 'School was successfully updated.'
-        format.html { redirect_to(url_for( :controller => :schools, :action => 'profile', :id=>@user)) }
+        format.html { redirect_to(@school) }
         format.xml  { head :ok }
       else
         format.html { render :action => "edit" }
@@ -95,34 +95,26 @@ class SchoolsController < ApplicationController
   end
   
   def profile_new
-    #@user = User.find(params[:id])
-    #@school = @user.person
     @school=School.find(params[:id])
     @user=@school.user
     @user_profile = UserProfile.new
-    respond_to do |format|
-      format.html # profile_new.html.erb
-      format.xml  { render :xml => @user_profile }
-    end
   end
-
+  
   def profile_create
-    #@user = User.find(params[:id])
-    #@school = @user.person
     @school=School.find(params[:id])
     @user=@school.user
-   @user_profile = UserProfile.new(params[:user_profile])
-   @user_profile.user=@user
-   respond_to do |format|
-      if @user_profile.save! && @user.update_attributes(params[:user]) && @school.update_attributes(params[:school])
-        flash[:notice] = 'Profile was successfully created.'
-        format.html { redirect_to(url_for( :controller => :schools, :action => 'profile_show', :id=>@school)) }
-       # format.xml  { render :xml => @user_profile, :status => :created, :location => @school }
-      else
-        format.html { render :action => "profile_new" }
-      #  format.xml  { render :xml => @user_profile.errors, :status => :unprocessable_entity }
-      end
+    @user_profile = UserProfile.new(params[:user_profile])
+    @user_profile.user=@user
+    User.transaction do
+      @school.update_attributes!(params[:school]) 
+      @user_profile.save!
+      @user.update_attributes!(params[:user])
     end
+    flash[:notice] = 'Profile was successfully created.'
+    redirect_to(url_for( :controller => :schools, :action => 'profile_show', :id=>@school))
+  rescue Exception => e
+    flash[:notice]="Error occured in profile creation: <br /> #{e.message}"
+    redirect_to(url_for( :controller => :schools, :action => 'profile_show', :id=>@school)) 
   end
   
   def profile_show
@@ -148,17 +140,16 @@ class SchoolsController < ApplicationController
     @school=School.find(params[:id])
     @user=@school.user
     @user_profile=@user.user_profile
-    respond_to do |format|
-      if @school.update_attributes(params[:school]) && @user.update_attributes(params[:user]) && @user_profile.update_attributes(params[:user_profile])
-        @user.user_profile = @user_profile
-        flash[:notice] = 'Profile was successfully updated.'
-        format.html { redirect_to(url_for( :controller => :schools, :action => 'profile_show', :id=>@school)) }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "profile_edit" }
-        format.xml  { render :xml => @school.errors, :status => :unprocessable_entity }
-      end
+    User.transaction do
+      @school.update_attributes!(params[:school]) 
+      @user_profile.update_attributes!(params[:user_profile])
+      @user.update_attributes!(params[:user])
     end
+    flash[:notice] = 'Profile was successfully updated.'
+    redirect_to(url_for( :controller => :schools, :action => 'profile_show', :id=>@school)) 
+  rescue Exception => e
+    flash[:notice]="Error occured in profile update: <br /> #{e.message}"
+    redirect_to(url_for( :controller => :schools, :action => 'profile_edit', :id=>@school)) 
   end
   
   def klasses
