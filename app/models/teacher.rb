56861df48e44d0a4c227b30b5a41d4c2f7ae774f
parvathy <parvathy@2498623e-5ac1-4a62-989a-52cda5f0dd9e>
2009-10-09 21:36:37 +0000

    order introduced for current_allotments so that while displaying teachers tab, the teacher-class allotment can be shown easily for each subject.

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -5,7 +5,7 @@ class Teacher < ActiveRecord::Base
   has_many :allotments, :class_name =>'TeacherAllotment' 
   #has_many :klasses, :through => :allotments
   has_many :subjects, :through => :allotments
-  has_many :current_allotments, :class_name => 'TeacherAllotment', :conditions => {:is_current => true}
+  has_many :current_allotments, :class_name => 'TeacherAllotment', :conditions => {:is_current => true}, :order => "subject_id"
   has_many :current_klasses, :through => :current_allotments, :source => :klass
   has_many :current_subjects, :through => :current_allotments, :source => :subject
   
