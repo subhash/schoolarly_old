10862600417e04f9162c3f49af9d108cd2df52c0
parvathy <parvathy@2498623e-5ac1-4a62-989a-52cda5f0dd9e>
2009-10-09 16:44:40 +0000

    Method to toggle between div show & hide.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,8 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def toggle_div(div)
+    update_page do |page|
+      page[div].toggle
+    end
+  end
 end
