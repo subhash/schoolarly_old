ac5ba06e3991bbd378c4fa024806cb02fe5de5b5
parvathy <parvathy@2498623e-5ac1-4a62-989a-52cda5f0dd9e>
2009-10-21 17:21:37 +0000

    Methods for teacher allotments & tabs added.

--- app/controllers/teachers_controller.rb
+++ app/controllers/teachers_controller.rb
@@ -1,7 +1,10 @@
 class TeachersController < ApplicationController
+  protect_from_forgery :only => [:destroy]
+  
   # GET /teachers
   # GET /teachers.xml
   def index
+    @active_tab = :Teachers
     @teachers = Teacher.all
     
     respond_to do |format|
@@ -13,6 +16,7 @@ class TeachersController < ApplicationController
   # GET /teachers/1
   # GET /teachers/1.xml
   def show
+    @active_tab = :Teachers
     @teacher = Teacher.find(params[:id])
     set_active_user(@teacher.user)
     respond_to do |format|
@@ -24,6 +28,7 @@ class TeachersController < ApplicationController
   # GET /teachers/new
   # GET /teachers/new.xml
   def new
+    @active_tab = :Teachers
     @teacher = Teacher.new
     
     respond_to do |format|
@@ -34,6 +39,7 @@ class TeachersController < ApplicationController
   
   # GET /teachers/1/edit
   def edit
+    @active_tab = :Teachers
     @teacher = Teacher.find(params[:id])
   end
   
@@ -74,6 +80,7 @@ class TeachersController < ApplicationController
   # DELETE /teachers/1
   # DELETE /teachers/1.xml
   def destroy
+    @active_tab = :Teachers
     @teacher = Teacher.find(params[:id])
     @teacher.destroy
     
@@ -82,4 +89,91 @@ class TeachersController < ApplicationController
       format.xml  { head :ok }
     end
   end
+  
+  def allotment_show
+    @active_tab = :Allotment
+    @teacher=Teacher.find(params[:id])
+    @user=@teacher.user
+    #set_active_user(@user)
+    @school=@teacher.school
+    @year = Klass.current_academic_year(@school)
+    @teacher_allotments=(@teacher.current_allotments).group_by{|allotment|allotment.subject_id}
+  end
+  
+  def list_add_allotment
+    @active_tab = :Allotment
+    @teacher=Teacher.find(params[:id])
+    @school=@teacher.school
+    @year = Klass.current_academic_year(@school)
+    @allotment_subjects=@school.subjects
+  end
+  
+  def allot_klasses
+    @teacher=Teacher.find(params[:id])
+    @subject=Subject.find(params[:subject_id])
+    @subject_id=params[:subject_id]
+    current_subject_allotments=@teacher.current_allotments.find_all_by_subject_id(params[:subject_id])
+    if current_subject_allotments.empty?
+      @allot_subject_klasses=(@teacher.school.klasses).group_by{|klass|klass.level}
+    else
+      allot_klasses=[]
+      current_subject_allotments.each {|allotment|
+        allot_klasses << allotment.klass
+      }
+      @allot_subject_klasses=(@teacher.school.klasses - allot_klasses).group_by{|klass|klass.level}
+    end
+    render :partial => "allot_klasses", :id=> @subject
+  end
+  
+  def add_allotments
+    @active_tab = :Allotment
+    @teacher=Teacher.find(params[:id])
+    subject_id=params[:subject_id]
+    add_klasses=params[:klasses].split(',')
+    add_klasses.each {|klass_id| 
+      if (!subject_id.empty? && !klass_id.empty?)
+        new_allotment=TeacherAllotment.new(:is_current => 1)
+        @subject=Subject.find(subject_id)
+        new_allotment.subject=@subject
+        new_allotment.klass=Klass.find(klass_id)
+        new_allotment.teacher=@teacher
+        new_allotment.save!
+      end
+    } 
+    @teacher_allotments=(@teacher.current_allotments).group_by{|allotment|allotment.subject_id}
+  end
+  
+  def list_delete_allotment
+    @active_tab = :Allotment
+    @teacher=Teacher.find(params[:id])
+    @user=@teacher.user
+    #set_active_user(@user)
+    @school=@teacher.school
+    @year = Klass.current_academic_year(@school)
+    @teacher_allotments=(@teacher.current_allotments).group_by{|allotment|allotment.subject_id}
+  end
+  
+  def delete_allotments
+    @active_tab = :Allotment
+    @teacher=Teacher.find(params[:id])
+    @allotment_ids=params[:allotments]
+    allotments=params[:allotments].split(',')
+    allotments.each {|allotment_id| 
+      if (!allotment_id.empty? && !allotment_id.nil?)
+        allotment=TeacherAllotment.find(allotment_id)
+        allotment.destroy
+      end
+    } 
+    @teacher_allotments=(@teacher.current_allotments).group_by{|allotment|allotment.subject_id}
+  end
+  
+  def self.tabs(teacher_id)
+    user_id=Teacher.find(teacher_id).user.id
+    tabs = [:Home => {:controller => :teachers, :action => 'show', :id=>teacher_id},#schools_path,
+    :Classes => {:controller => :teachers, :action => 'klasses', :id=>teacher_id},
+    :Teachers => {:controller => :teachers, :action => 'teacher_allotment', :id=>teacher_id} ,#'teachers_path',
+    :Profile =>  {:controller => :user_profiles, :action => 'show', :id=>user_id}]
+    return tabs
+  end
+  
 end
