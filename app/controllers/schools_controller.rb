a1c67a35c81ba4c11521fae80586e8befa3da322
parvathy <parvathy@2498623e-5ac1-4a62-989a-52cda5f0dd9e>
2009-10-27 05:17:11 +0000

    add_school_teacher commented & add_teacher,create_teacher,new_teacher added.

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -107,15 +107,74 @@ class SchoolsController < ApplicationController
     @klasses = (Klass.current_klasses(@school, @year)).group_by{|klass|klass.level}
   end
   
-  def add_school_teacher
-    @school=School.find(params[:id])
-    @user = User.find(:first, :conditions => ["email = ?",params[:user][:email]])
-    if @user.nil?
-      flash[:notice] = "The user does not exist"
-    else
-      @school.teachers << @user.person
-    end
+#  def add_school_teacher
+#    @school=School.find(params[:id])
+#    @user = User.find(:first, :conditions => ["email = ?",params[:user][:email]])
+#    if @user.nil?
+#      flash[:notice] = "The user does not exist"
+#    else
+#      @school.teachers << @user.person
+#    end
+#  end
+  
+   def new_teacher
+    @active_tab = :Teachers
+    @user = User.new
+    @school = School.find(params[:id])
+    render :update do |page|      
+      page.replace_html ("new_teacher_form", :partial => "schools/new_teacher")
+      page[:new_teacher_form].show
+    end        
   end
+  
+  def create_teacher
+    @school = School.find(params[:school_id])
+    @user = User.new(params[:user])
+    if(@user.save)
+      @teacher = Teacher.new
+      @teacher.user = @user
+      @user.person = @teacher
+      @teacher.school = @school
+      if @teacher.save
+        @teachers = @school.teachers
+        flash[:notice] = "Account registered!"        
+        render :update do |page|
+          page.select("form").first.reset
+          page[:new_teacher_form].hide
+          page.insert_html :bottom, 'teachers', :partial => "teacher_user", :object => @user
+        end
+      else
+        render :action => :new
+      end
+    else
+      render :action => :new
+    end  
+  end
+
+  def add_teacher    
+    @school = School.find(params[:school_id])
+    if(params[:email])
+      @user = User.find_by_email_and_person_type(params[:email], 'Teacher')
+      if @user.nil?
+        render :update do |page|
+          page[:password].show
+          page[:add_teacher_button].disable
+        end
+      else
+        @teacher = @user.person
+        @teacher.school = @school
+        if @teacher.save
+          @teachers = @school.teachers
+          flash[:notice] = "Account registered!"
+          render :update do |page|
+            page.select("form").first.reset
+            page[:new_teacher_form].hide
+            page.insert_html :bottom, 'teachers', :partial => "teacher_user", :object => @user
+          end
+        end
+      end
+    end
+  end 
   
   def auto_complete_for_user_email(email, person_type)
     find_options = { 
@@ -204,7 +263,7 @@ class SchoolsController < ApplicationController
     end  
   end
   
-  def remove_student
+    def remove_student
     @school = School.find(params[:id])
     @student = Student.find(params[:student_id])
     @school.students.delete(@student)
