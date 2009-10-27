c1fea93a36ff1701eff24b281daa9392eb8da1b6
parvathy <parvathy@2498623e-5ac1-4a62-989a-52cda5f0dd9e>
2009-10-27 08:34:54 +0000

    migrate script for exam_types master data

---
+++ db/migrate/20091027081719_add_default_exam_types_data.rb
@@ -0,0 +1,24 @@
+class AddDefaultExamTypesData < ActiveRecord::Migration
+  def self.up
+    ExamType.create(:name => "Daily", :description => "Daily Test")
+    ExamType.create(:name => "Weekly", :description => "Weekly Test")
+    ExamType.create(:name => "Monthly", :description => "Monthly Test")
+    ExamType.create(:name => "Quarterly", :description => "Quarterly Test")
+    ExamType.create(:name => "Annual", :description => "Annual Exam")
+    ExamType.create(:name => "First Term", :description => "First Term Test")
+    ExamType.create(:name => "Mid Term", :description => "Mid Term Test")
+    ExamType.create(:name => "Last Term", :description => "Last Term Test")
+    ExamType.create(:name => "Semester", :description => "Semester Test")
+    ExamType.create(:name => "Class", :description => "Class Test")
+    ExamType.create(:name => "Unit", :description => "Unit Test")
+    ExamType.create(:name => "Model", :description => "Model Exam")
+    ExamType.create(:name => "Pre-Model", :description => "Pre-Model Test")
+    ExamType.create(:name => "Board", :description => "Board Exam")
+    ExamType.create(:name => "Supplimentary", :description => "Supplimentary Exam")
+    ExamType.create(:name => "Others", :description => "Uncategorised")    
+  end
+
+  def self.down
+    ExamType.delete_all
+  end
+end
