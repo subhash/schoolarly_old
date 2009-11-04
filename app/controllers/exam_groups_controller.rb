fcc8cbd468d98de607e09f75775b805467cbbe12
parvathy <parvathy@2498623e-5ac1-4a62-989a-52cda5f0dd9e>
2009-11-04 10:45:12 +0000

    exam_groups controller

---
+++ app/controllers/exam_groups_controller.rb
@@ -0,0 +1,2 @@
+class ExamGroupsController < ApplicationController
+end
---
+++ test/functional/exam_groups_controller_test.rb
@@ -0,0 +1,8 @@
+require 'test_helper'
+
+class ExamGroupsControllerTest < ActionController::TestCase
+  # Replace this with your real tests.
+  test "the truth" do
+    assert true
+  end
+end
