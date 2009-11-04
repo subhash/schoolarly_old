5fca05b3099379e0e9052ad3b9a83ae26dd40ef7
parvathy <parvathy@2498623e-5ac1-4a62-989a-52cda5f0dd9e>
2009-11-04 16:04:47 +0000

    in-place editing with validation

---
+++ lib/custom_in_place_editing.rb
@@ -0,0 +1,37 @@
+module ActionController
+  module Macros
+   module CustomInPlaceEditing
+    def self.included(base)
+     base.extend(ClassMethods)
+    end
+   
+    module ClassMethods
+     def in_place_edit_with_validation_for(object, attribute)
+      define_method("set_#{object}_#{attribute}") do
+       klass = object.to_s.camelize.constantize
+      @item = klass.find(params[:id])
+      @item.send("#{attribute}=", params[:value])
+       if @item.save
+        render :update do |page| 
+         page.replace_html("#{object}_#{attribute}_#{params[:id]}_in_place_editor", 
+         @item.send(attribute))
+        end
+      else
+        render :update do |page|
+         page.alert(@item.errors.full_messages.join("\n"))
+         klass.query_cache.clear_query_cache if klass.method_defined?:query_cache
+         @item.reload
+         page.replace_html("#{object}_#{attribute}_#{params[:id]}_in_place_editor", 
+         @item.send(attribute))
+        end
+       end
+      end
+     end
+    end
+   end
+  end
+end
+
+ActionController::Base.class_eval do
+  include ActionController::Macros::CustomInPlaceEditing
+end
