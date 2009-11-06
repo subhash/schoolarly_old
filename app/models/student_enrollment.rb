39754ecb95121ba8c7c1791fc8e38c629ecbbc20
shenuja <shenuja@2498623e-5ac1-4a62-989a-52cda5f0dd9e>
2009-11-06 10:55:15 +0000

    removing validation for roll number

--- app/models/student_enrollment.rb
+++ app/models/student_enrollment.rb
@@ -3,6 +3,4 @@ class StudentEnrollment < ActiveRecord::Base
   belongs_to :klass
   has_one :current_student, :class_name => 'Student', :foreign_key => 'current_enrollment_id'
   has_and_belongs_to_many :subjects
-  
-  validates_uniqueness_of :roll_number, :scope => [:klass_id]
 end
