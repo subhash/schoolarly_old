bb580c1a0bad99aee32d3ea18d5e4c70666678b4
parvathy <parvathy@2498623e-5ac1-4a62-989a-52cda5f0dd9e>
2009-11-26 10:29:34 +0000

    current_subject_klasses is added for teachers allotment

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -3,4 +3,9 @@ class Subject < ActiveRecord::Base
   has_and_belongs_to_many :klasses
   has_many :exams
   has_and_belongs_to_many :student_enrollments
+  
+  def current_subject_klasses(school_id)
+    return self.klasses.find(:all, :conditions => ["year=?",Klass.current_academic_year(school_id)])
+  end
+  
 end
