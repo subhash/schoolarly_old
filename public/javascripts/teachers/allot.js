e4a5cdafdce8f2c38f27587c66754b28dea5990c
parvathy <parvathy@2498623e-5ac1-4a62-989a-52cda5f0dd9e>
2009-12-01 17:32:32 +0000

    Javascript for showing klasses for any subject in allotment is corrected & moved out of the view to allot.js.

--- public/javascripts/teachers/allot.js
+++ public/javascripts/teachers/allot.js
@@ -1,11 +1,17 @@
-	function readySelectable(div_id){
-        jQuery("#" + div_id ).bind('selectablestop', function(event, ui){
+	function readySelectable(subject_id){
+		div_id = subject_id + "-klasses_selectable";
+        jQuery(div_id ).bind('selectablestop', function(event, ui){
             klass_indices = "";
-            jQuery("#" + div_id + " " + 'td.ui-selected').each(function(i){
+            jQuery(div_id + " " + 'td.ui-selected').each(function(i){
                 id = this.id;
                 klass_indices += "," + id;
+				alert(klass_indices);
             });
-        });		
+        });
 }
 
-
+	function showKlasses(subject_id){
+		hideShowDivs("#klasses .klass-div", "#" + subject_id + "-klasses");
+		readySelectable("#" + subject_id);
+	}
+	
