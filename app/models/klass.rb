4b108ae1de0848f015c2ec5119b71b76190ec7d2
parvathy <parvathy@2498623e-5ac1-4a62-989a-52cda5f0dd9e>
2009-12-10 09:49:50 +0000

    exams method added

--- app/models/klass.rb
+++ app/models/klass.rb
@@ -2,17 +2,21 @@ class Klass < ActiveRecord::Base
   named_scope :current_klasses, lambda { |school_id, year|
     { :conditions => { :school_id => school_id , :year => year } , :order => "level, division"}
   }
-  
+
   belongs_to :school
   belongs_to :class_teacher, :class_name => 'Teacher', :foreign_key => 'teacher_id'
   has_many :enrollments, :class_name =>'StudentEnrollment'  
   has_many :students, :through => :enrollments
-  has_and_belongs_to_many :subjects, :order => "name" 
+  has_and_belongs_to_many :subjects, :order => "name"
   has_many :teacher_allotments 
   has_many :teachers, :through => :teacher_allotments, :uniq => true
-  has_many :exam_groups
+	has_many :exam_groups
   
   validates_uniqueness_of :division, :scope => [:school_id, :level, :year]
+  
+  def exams
+    return (self.exam_groups).collect{|eg| (!eg.exams.empty?)?(eg.exams):(Exam.new(:exam_group_id => eg.id))}.flatten
+  end
   
   def current_students
    (self.enrollments.select{|e| e.current_student}).collect{|s|s.student}
