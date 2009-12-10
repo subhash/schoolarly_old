8d891e38086db3da18df7cc208209859bbd148d3
parvathy <parvathy@2498623e-5ac1-4a62-989a-52cda5f0dd9e>
2009-12-10 09:40:04 +0000

    methods renamed & all javascript code moved to rjs

--- app/controllers/exam_groups_controller.rb
+++ app/controllers/exam_groups_controller.rb
@@ -1,114 +1,57 @@
 class ExamGroupsController < ApplicationController
   
-  def remove_exam_group
+  def destroy
     @active_tab = :Exams
-    @exam_group=ExamGroup.find(params[:id])
-    klass=@exam_group.klass
-    div_name=klass.id.to_s + '_content_table'
-    Exam.delete(@exam_group.exams)
-    @exam_group.save!
-    @exam_group.destroy
-    if klass.exam_groups.empty?
-      render :update do |page|      
-        page.replace_html(div_name, :text => "<blockquote>No exam group added yet</blockquote>")
-        page.replace_html("exams_index_div", :text => "")
-      end
-    else
-      render :update do |page|      
-        page.replace_html(div_name, :partial => 'exam_group', :collection => klass.exam_groups)
-        page.replace_html("exams_index_div", :text => "")
-      end
-    end
-    
+    exam_group=ExamGroup.find(params[:id])
+    @klass=exam_group.klass
+    div_name=@klass.id.to_s + '_content_table'
+    Exam.delete(exam_group.exams)
+    exam_group.save!
+    exam_group.destroy
+    respond_to do |format|
+      flash[:notice] = 'Exam group was successfully removed.'
+      format.js {render :template => 'exam_groups/destroy'}
+    end 
   end
   
-  def add_exam_group_dialog_show
-    klass=Klass.find(params[:id])
-    @exam_types=ExamType.find(:all)
-    render :update do |page|
-      page.call 'jQuery.noConflict'
-      page.replace_html("add_exam_group", :partial =>'exam_groups/new' , :object=>klass)
-      page << "jQuery('#dialog_add_exam_group').dialog({
-            bgiframe: true,
-            height: 198,
-            width: 360,
-            modal: true,
-            autoOpen: false
-        });"
-      page << "jQuery('#dialog_add_exam_group').dialog('open');"   
-    end
-  end
-  
-  def add_exam_group
-    klass=Klass.find(params[:id])
-    exam_group=ExamGroup.new()
-    exam_group.exam_type=ExamType.find(params[:exam_type])
-    exam_group.description=params[:description]
-    klass.exam_groups << exam_group
-    klass.save!
-    div_name=klass.id.to_s + '_content_table'
-    if klass.exam_groups.count==1
-      render :update do |page|
-        page << "jQuery('#dialog_add_exam_group').dialog('close');"
-        page.replace_html(div_name, :partial =>'exam_group', :object => exam_group)
-      end
-    else
-      render :update do |page|
-        page << "jQuery('#dialog_add_exam_group').dialog('close');"
-        page.insert_html(:bottom, div_name, :partial =>'exam_group', :object => exam_group)
-      end
-    end    
+  def create
+    @klass=Klass.find(params[:id])
+    @exam_group=ExamGroup.new()
+    @exam_group.exam_type=ExamType.find(params[:exam_type])
+    @exam_group.description=params[:description]
+    @klass.exam_groups << @exam_group
+    @klass.save!
+    respond_to do |format|
+      flash[:notice] = 'Exam group was successfully created.'
+      format.js {render :template => 'exam_groups/create_success'}
+    end 
   rescue Exception => e
-    flash[:notice]="Error occured in exam group add: <br /> #{e.message}"
-    render :update do |page|
-      page << "jQuery('#dialog_add_exam_group').dialog('close');"
-      page.replace_html("exams_index_div", :text => flash[:notice])
-    end
+    respond_to do |format|
+      format.js {render :template => 'exam_groups/create_error'}
+    end 
   end
 
-  def edit_exam_group_dialog_show
-    exam_group=ExamGroup.find(params[:exam_group_id])
-    @exam_types=ExamType.find(:all)
-    render :update do |page|
-      page.call 'jQuery.noConflict'
-      page.replace_html("edit_exam_group", :partial =>'exam_groups/edit' , :object=>exam_group)
-      page << "jQuery('#dialog_edit_exam_group').dialog({
-            bgiframe: true,
-            height: 198,
-            width: 360,
-            modal: true,
-            autoOpen: false
-        });"
-      page << "jQuery('#dialog_edit_exam_group').dialog('open');"   
-    end
-  end
-  
-  def update_exam_group
-    exam_group=ExamGroup.find(params[:id])
-    exam_group.exam_type=ExamType.find(params[:exam_type])
-    exam_group.description=params[:description]
-    exam_group.save!
-    div_name=exam_group.klass.id.to_s + '_content_table'
-    exam_groups= exam_group.klass.exam_groups
-    render :update do |page|
-      page << "jQuery('#dialog_edit_exam_group').dialog('close');"
-      page.replace_html(div_name, :partial =>'exam_group', :collection => exam_groups)
-    end
+  def update
+    @exam_group=ExamGroup.find(params[:id])
+    @exam_group.exam_type=ExamType.find(params[:exam_type])
+    @exam_group.description=params[:description]
+    @exam_group.save!
+    respond_to do |format|
+      flash[:notice] = 'Exam group was successfully updated.'
+      format.js {render :template => 'exam_groups/update_success'}
+    end 
   rescue Exception => e
-    flash[:notice]="Error occured in exam group edit: <br /> #{e.message}"
-    render :update do |page|
-      page << "jQuery('#dialog_edit_exam_group').dialog('close');"
-      page.replace_html("exams_index_div", :text => flash[:notice])
-    end
+    respond_to do |format|
+      format.js {render :template => 'exam_groups/update_error'}
+    end 
   end  
   
   def show
-    @active_tab = :Exams
     @exam_group=ExamGroup.find(params[:id])
     @subjects=Subject.find(:all)
-
+    @exams=@exam_group.exams.group_by{|e| e.exam_group}
     render :update do |page|
-      page.replace_html("exams_index_div", :partial => "exams/exams", :object => @exam_group)
+      page.replace_html("exams_index_div", :partial => "exams/exams", :object => @exams)
     end      
   end
   
