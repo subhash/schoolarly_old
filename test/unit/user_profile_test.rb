fadbaae47c5bb7fc5cd23b97601f52e3878c6af7
parvathy <parvathy@2498623e-5ac1-4a62-989a-52cda5f0dd9e>
2010-01-11 10:49:14 +0000

    2 new test cases added

--- test/unit/user_profile_test.rb
+++ test/unit/user_profile_test.rb
@@ -1,8 +1,31 @@
 require 'test_helper'
 
 class UserProfileTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  
+  def setup
+    @paru=users(:paru)
   end
+  
+  test "a user can have only one profile & a profile belongs to one & only 1 user" do
+    
+  end
+  
+  test "cannot create a profile without first & last names" do
+    assert_nil @paru.user_profile
+    paru_profile = UserProfile.new(:middle_name =>'Middle')
+    paru_profile.user = @paru
+    paru_profile.save
+    assert !paru_profile.valid?
+  end
+  
+  
+  test "profile-create" do
+    assert_nil @paru.user_profile
+    paru_profile = UserProfile.new(:first_name =>'First', :last_name =>'Last', :user => @paru)
+    assert paru_profile.valid?
+    paru_profile.save!
+    assert paru_profile.valid?
+    assert_equal 'First Last', paru_profile.name.to_s
+  end
+  
 end
