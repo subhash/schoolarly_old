84b55703c4fd65d62918da024277f3501da2c935
parvathy <parvathy@2498623e-5ac1-4a62-989a-52cda5f0dd9e>
2010-01-20 11:27:02 +0000

    test "delete teacher allotment" marked as TODO.

--- test/functional/teacher_allotments_controller_test.rb
+++ test/functional/teacher_allotments_controller_test.rb
@@ -1,8 +1,9 @@
 require 'test_helper'
 
 class TeacherAllotmentsControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  
+  test "delete teacher allotment" do
+    #TODO
   end
+  
 end
