699331e9e5c433c1bd0660e27dcbba4fcf97794d
parvathy <parvathy@2498623e-5ac1-4a62-989a-52cda5f0dd9e>
2010-01-25 11:07:35 +0000

    has_many subjects, :through => exams added

--- app/models/exam_group.rb
+++ app/models/exam_group.rb
@@ -3,7 +3,7 @@ class ExamGroup < ActiveRecord::Base
   belongs_to :klass
   
   has_many :exams , :dependent => :destroy
-  
+  has_many :subjects, :through => :exams
 #  validates_presence_of :description, :message => "cannot be blank."
 #  validates_uniqueness_of :description, :scope => [:klass_id], :message => "of exam group cannot be repeated for the same class."
 
