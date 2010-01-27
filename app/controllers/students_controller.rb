2ec5537918cdbb548d953f464d668e780cb461c0
parvathy <parvathy@2498623e-5ac1-4a62-989a-52cda5f0dd9e>
2010-01-27 20:35:23 +0000

    @exams introduced in show method

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -45,6 +45,7 @@ class StudentsController < ApplicationController
       add_js_page_action(:title => 'Add to school', :render => {:partial =>'students/add_to_school_form', :locals => {:student => @student, :schools => School.find(:all)}})
     end
     add_breadcrumb(@student.name)
+    @exams=@student_enrollment.exams.group_by{|e| e.exam_group}
   end
   # GET /students/new
   # GET /students/new.xml
