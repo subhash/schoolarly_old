ebe8a526e07d19ad31a6dda5fa51c5aa5005a0e0
parvathy <parvathy@2498623e-5ac1-4a62-989a-52cda5f0dd9e>
2010-01-27 20:36:12 +0000

    exams method introduced

--- app/models/student_enrollment.rb
+++ app/models/student_enrollment.rb
@@ -3,4 +3,9 @@ class StudentEnrollment < ActiveRecord::Base
   belongs_to :klass
   has_one :current_student, :class_name => 'Student', :foreign_key => 'current_enrollment_id', :dependent => :nullify
   has_and_belongs_to_many :subjects, :order => "name"
+  
+  def exams
+    return Exam.all(:conditions => ["exam_group_id IN (:egid) AND subject_id IN (:sid)", {:egid => self.klass.exam_groups.collect{|eg| eg.id}, :sid => self.subjects}]).flatten
+  end
+  
 end
