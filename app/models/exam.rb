c6bbf999b3bbbf72b36771228414cd00d13b1ad9
parvathy <parvathy@2498623e-5ac1-4a62-989a-52cda5f0dd9e>
2010-02-01 17:04:51 +0000

    to_s method defined

--- app/models/exam.rb
+++ app/models/exam.rb
@@ -6,5 +6,10 @@ class Exam < ActiveRecord::Base
   has_one :klass, :through => :exam_group
   has_one :exam_type, :through => :exam_group
   
+  def to_s
+    return self.subject.name + ' ' + exam_group.exam_type.description + ' for ' + exam_group.klass.name + ': ' + exam_group.description 
+  end
+  
   acts_as_authorizable
+  
 end
