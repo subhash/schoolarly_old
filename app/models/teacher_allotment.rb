a021565e068c3778bbcddb50b17f11ee4969585f
parvathy <parvathy@2498623e-5ac1-4a62-989a-52cda5f0dd9e>
2010-02-02 09:12:33 +0000

    teacher_allotment is split into teacher_subject_allotment & teacher_klass_allotment.

--- app/models/teacher_allotment.rb
+++
@@ -1,9 +0,0 @@
-class TeacherAllotment < ActiveRecord::Base
-  belongs_to :teacher
-  belongs_to :subject
-  belongs_to :klass
-  
-  named_scope :current_for_klass, lambda { |klass_id|
-    { :conditions => { :klass_id => klass_id , :is_current => true } , :order => "subject_id"}
-  }
-end
