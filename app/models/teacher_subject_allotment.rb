f2254fa41951ac539f6fc3f7539ee51e4254e02f
parvathy <parvathy@2498623e-5ac1-4a62-989a-52cda5f0dd9e>
2010-02-04 20:54:50 +0000

    has_many :current_klass_allotments & has_many :current_klasses defined

--- app/models/teacher_subject_allotment.rb
+++ app/models/teacher_subject_allotment.rb
@@ -3,5 +3,7 @@ class TeacherSubjectAllotment < ActiveRecord::Base
   belongs_to :school
   belongs_to :subject
   has_many :teacher_klass_allotments
+  has_many :current_klass_allotments, :class_name => 'TeacherKlassAllotment', :conditions => [' end_date IS NULL ' ]
   has_many :klasses, :through => :teacher_klass_allotments, :class_name => 'Klass'
+  has_many :current_klasses, :through => :current_klass_allotments, :source => :klass
 end
