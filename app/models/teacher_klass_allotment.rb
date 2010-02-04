a81e7f6a3a35cd0bb5b83cf9154faa72df77c341
parvathy <parvathy@2498623e-5ac1-4a62-989a-52cda5f0dd9e>
2010-02-04 19:44:21 +0000

    validation added

--- app/models/teacher_klass_allotment.rb
+++ app/models/teacher_klass_allotment.rb
@@ -1,4 +1,8 @@
 class TeacherKlassAllotment < ActiveRecord::Base
   belongs_to :teacher_subject_allotment
   belongs_to :klass
+  
+  validates_presence_of :teacher_subject_allotment
+  validates_presence_of :klass
+  
 end
