f03b34433f79509163be29ce128a4eb5ffce63b9
parvathy <parvathy@2498623e-5ac1-4a62-989a-52cda5f0dd9e>
2010-02-05 09:46:23 +0000

    test created for exam_group_controller

--- test/functional/exam_groups_controller_test.rb
+++ test/functional/exam_groups_controller_test.rb
@@ -1,8 +1,22 @@
 require 'test_helper'
 
 class ExamGroupsControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+
+  def setup
+    activate_authlogic
+    @mal = subjects(:malayalam)
+    @eng = subjects(:english)
+    @stTeresas = schools(:st_teresas)
+    @one_A=klasses(:one_A)
+    @exam_type = exam_types(:half_yearly)
+    UserSession.create(@stTeresas.user)
   end
+  
+  test "should create exam group" do
+    assert_difference ('ExamGroup.count', 1) do
+      post :create, :exam_group => {:exam_type_id => @exam_type.to_param, :klass_id => @one_A.to_param}, :exam => {:subject_ids => [@eng.to_param, @mal.to_param]}, :entity_class => 'Klass', :entity_id => @one_A
+    end
+    assert_response :success
+  end
+  
 end
