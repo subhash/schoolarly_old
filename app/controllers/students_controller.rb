c35f06311bb90122c7b9b400e4e53ebe1292741e
parvathy <parvathy@2498623e-5ac1-4a62-989a-52cda5f0dd9e>
2010-02-10 06:49:34 +0000

    allot_teacher_new method added.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -108,11 +108,17 @@ class StudentsController < ApplicationController
     end
   end
   
+  def allot_teacher_new
+    @subject = Subject.find(params[:subject])
+    @klass = Klass.find(params[:klass])
+    respond_to do |format|
+      format.js {render :template => 'teacher_klass_allotments/allot_teacher_new'}
+    end
+  end
+    
   def allot_teacher
     @teacher_klass_allotment = TeacherKlassAllotment.new(:klass_id => params[:klass_id])
     @klass = Klass.find(params[:klass_id])
-    puts "subject is ==="
-    puts Subject.find(params[:subject_id]).inspect
     teacher_subject_allotment = TeacherSubjectAllotment.find(:first, :conditions => ["teacher_id = ? and subject_id = ?", params[:teacher_id], params[:subject_id]])
     @teacher_klass_allotment.teacher_subject_allotment = teacher_subject_allotment
     @teacher_klass_allotment.klass = @klass
