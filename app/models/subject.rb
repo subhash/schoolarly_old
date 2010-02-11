097722649a5298f8294d14acee997c5fe04b6b9d
parvathy <parvathy@2498623e-5ac1-4a62-989a-52cda5f0dd9e>
2010-02-11 09:30:17 +0000

    :school_id was missing in the condition for ofSchool(school_id) method

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -2,7 +2,7 @@ class Subject < ActiveRecord::Base
  # has_and_belongs_to_many :schools
   has_and_belongs_to_many :klasses do
     def ofSchool(school_id)
-      find :all, :conditions => ['year = ? ', (Klass.maximum :year, :conditions => {:school_id => school_id})], :order => "level, division"
+      find :all, :conditions => ['year = ? AND school_id = ?', (Klass.maximum :year, :conditions => {:school_id => school_id}), school_id], :order => "level, division"
     end
   end
   has_many :exams
