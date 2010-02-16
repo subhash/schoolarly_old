81dd15d6f01b546a679c948b10d67664cd320a9e
parvathy <parvathy@2498623e-5ac1-4a62-989a-52cda5f0dd9e>
2010-02-16 07:08:45 +0000

    is_collection_empty? helper method is added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,15 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  
   def toggle_div(div)
     update_page do |page|
       page[div].toggle
+    end
+  end
+  
+  def is_collection_empty?(collection, message)
+    if collection.nil? || collection.empty?
+      render :text => '<br/><blockquote>' + message + '</blockquote>'
     end
   end
   
