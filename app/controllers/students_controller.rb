b74ab8eaec3ec1822fbafaf053d4d1b8564e0170
parvathy <parvathy@2498623e-5ac1-4a62-989a-52cda5f0dd9e>
2010-03-26 09:20:19 +0000

    @exam_groups in show modified

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -31,7 +31,7 @@ class StudentsController < ApplicationController
         @all_subjects = Subject.find(:all, :order => 'name')
         add_breadcrumb(@klass.name,@klass)
         add_js_page_action(:title => 'Add/Remove Subjects',:render => {:partial => 'papers/edit_papers_form', :locals => {:entity => @student, :papers => @klass.papers}})
-        @exam_groups = @klass.exams.collect{|exam| exam.exam_group}.uniq
+        @exam_groups = @klass.exam_groups.select{|eg| (eg.subjects & @student.subjects).any?}
       else
         @klasses = @school.klasses
         add_js_page_action(:title => 'Assign Class', :render => {:partial => 'students/add_to_klass_form', :locals => {:student => @student}})
