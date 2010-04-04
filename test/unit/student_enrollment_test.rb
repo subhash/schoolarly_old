98345e5036e13688c9e30844fd3b8c4c84858548
parvathy <parvathy@2498623e-5ac1-4a62-989a-52cda5f0dd9e>
2010-04-04 16:32:58 +0000

    commented all the tests

--- test/unit/student_enrollment_test.rb
+++ test/unit/student_enrollment_test.rb
@@ -1,29 +1,29 @@
 require 'test_helper'
 
 class StudentEnrollmentTest < ActiveSupport::TestCase
-  def setup
-    @shenu = students :shenu
-    @klass = klasses(:two_B)
-    @shenu_enr = student_enrollments :shenu_one
-    @mal = subjects(:malayalam)
-    @eng = subjects(:english)
-  end
-  
-  test "student-enrollment relationship" do
-    @shenu_enr.klass = @klass
-    @shenu_enr.student = @shenu
-    @shenu.current_enrollment = @shenu_enr
-    @shenu.save
-    assert_equal 1, @shenu.enrollments.size
-  end
-  
-  test "student-enrollments subject relationship" do
-    @shenu.current_enrollment = @shenu_enr
-    assert @shenu.save
-    @shenu_enr.subjects << @mal
-    @shenu_enr.subjects << @eng
-    assert_equal 2, @shenu.current_subjects.size
-  end
+#  def setup
+#    @shenu = students :shenu
+#    @klass = klasses(:two_B)
+#    @shenu_enr = student_enrollments :shenu_one
+#    @mal = subjects(:malayalam)
+#    @eng = subjects(:english)
+#  end
+#  
+#  test "student-enrollment relationship" do
+#    @shenu_enr.klass = @klass
+#    @shenu_enr.student = @shenu
+#    @shenu.current_enrollment = @shenu_enr
+#    @shenu.save
+#    assert_equal 1, @shenu.enrollments.size
+#  end
+#  
+#  test "student-enrollments subject relationship" do
+#    @shenu.current_enrollment = @shenu_enr
+#    assert @shenu.save
+#    @shenu_enr.subjects << @mal
+#    @shenu_enr.subjects << @eng
+#    assert_equal 2, @shenu.current_subjects.size
+#  end
   
   
 end
