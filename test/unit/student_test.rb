ecc615b1c4fb35dc7b5fb8078239b7e16020133b
shenuja <shenuja@2498623e-5ac1-4a62-989a-52cda5f0dd9e>
2010-04-07 15:48:00 +0000

    Adding student tests

--- test/unit/student_test.rb
+++ test/unit/student_test.rb
@@ -5,6 +5,10 @@ class StudentTest < ActiveSupport::TestCase
     @school = schools(:st_teresas)
     @user = users(:mary)
     @paru = students(:paru)
+    @shenu = students :shenu
+    @klass = klasses(:two_B)
+    @mal = papers(:one_A_malayalam_sunil)
+    @eng = papers(:one_A_english_sunil)
   end
   
   test "student-user association" do
@@ -17,12 +21,39 @@ class StudentTest < ActiveSupport::TestCase
   
   test "student-school association" do 
     assert_equal @paru.school, @school
-    assert_equal 3, @school. students.size
-    @school.students.delete(@paru)
-    @school.save!
+    assert_difference('@school.students.size', -1) do 
+      @school.students.delete(@paru)
+      @school.save!
+    end
     # TODO revisit the reload - why doesnt it work otherwise?
     @paru.reload
     assert_nil @paru.school
-    assert_equal 2, @school.students.size
+  end
+  
+  test "student-klass association" do
+    assert_difference('@klass.reload.students.size', 2) do
+      @shenu.klass = @klass
+      @shenu.save!
+      @klass.students << @paru
+      @klass.save!
+    end
+    assert_difference('@klass.reload.students.size', -1) do
+      @shenu.klass = nil
+      @shenu.save!
+    end
+  end
+  
+  test "student-subjects association" do
+    @one_A =  klasses(:one_A)
+    @shenu.klass = @one_A
+    assert_difference('@shenu.papers.size', 2) do
+      @shenu.papers << @eng
+      @shenu.papers << @mal
+      @shenu.save!
+    end
+    assert_difference('@shenu.reload.papers.size', -1) do
+      @one_A.papers.destroy(@eng)
+      @one_A.save!
+    end
   end
 end
