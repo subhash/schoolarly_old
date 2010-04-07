d1d92b0e60fd24faf75866faa47bef398915567a
shenuja <shenuja@2498623e-5ac1-4a62-989a-52cda5f0dd9e>
2010-04-07 15:08:52 +0000

    Adding paper tests

--- test/unit/paper_test.rb
+++ test/unit/paper_test.rb
@@ -1,8 +1,29 @@
 require 'test_helper'
 
 class PaperTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  def setup
+    @school=schools(:st_teresas)
+    @mal=subjects(:malayalam)
+    @eng=subjects(:english)  
+    @klass = klasses(:two_B)
+  end
+  
+  test "school-paper-relationship" do
+    #    @klass.papers << Paper.new(:subject => @mal)
+    #    @klass.subjects <<  Paper.new(:subject => @eng)
+    #    assert_equal 2,@school.papers.size
+  end
+  
+  test "klass-paper-relationship" do
+    assert_difference('@klass.papers.size') do
+      @klass.papers << Paper.new(:subject => @mal)
+    end
+    @klass.papers <<  Paper.new(:subject => @eng)
+    assert @klass.save!
+    assert @klass.subjects.include?(@mal)
+    @klass.papers << Paper.new(:subject => @eng)
+    assert_raise ActiveRecord::RecordInvalid do
+      @klass.save!
+    end
   end
 end
