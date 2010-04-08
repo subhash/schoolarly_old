0418d938855b6e9cbaf6c50014e26c4dd6007f26
parvathy <parvathy@2498623e-5ac1-4a62-989a-52cda5f0dd9e>
2010-04-08 19:39:13 +0000

    teacher is also displayed for each exam

--- app/helpers/exam_groups_helper.rb
+++ app/helpers/exam_groups_helper.rb
@@ -6,11 +6,11 @@ module ExamGroupsHelper
   
   def get_show_options(entity)
     options = case entity.class.name
-          when 'School'             then [:show_klass, :edit_exam_group, :delete_exam_group, :add_exam, :edit_exam, :delete_exam, :view_scores]
-          when 'Klass'              then [:edit_exam_group, :delete_exam_group, :add_exam, :edit_exam, :delete_exam, :view_scores]
-          when 'Student'            then [:edit_exam, :show_score]
+          when 'School'             then [:show_klass, :edit_exam_group, :delete_exam_group, :add_exam, :edit_exam, :delete_exam, :view_scores, :show_teacher]
+          when 'Klass'              then [:edit_exam_group, :delete_exam_group, :add_exam, :edit_exam, :delete_exam, :view_scores, :show_teacher]
+          when 'Student'            then [:edit_exam, :show_score, :show_teacher]
           when 'Teacher'            then [:show_klass, :add_exam, :edit_exam, :delete_exam, :view_scores]
-          when 'Exam'               then [:edit_exam, :view_scores]
+          when 'Exam'               then [:edit_exam, :view_scores, :show_teacher]
     end
     return options
   end
