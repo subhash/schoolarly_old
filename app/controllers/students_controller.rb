ba1accd2b689a1974592d62c4600ddef04f05b5a
parvathy <parvathy@2498623e-5ac1-4a62-989a-52cda5f0dd9e>
2010-04-15 10:26:54 +0000

    Only the exam with scores will be shown in the exams tab of students page & the tab name is changed to scores.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -29,7 +29,7 @@ class StudentsController < ApplicationController
         @all_subjects = Subject.find(:all, :order => 'name')
         add_breadcrumb(@klass.name,@klass)
         add_js_page_action(:title => 'Add/Remove Subjects',:render => {:partial => 'papers/edit_papers_form', :locals => {:entity => @student, :papers => @klass.papers}})
-        @exams = @student.klass.exams.select{|e| @student.subjects.include?(e.subject)}
+        @exams = @student.exams
       else
         @klasses = @school.klasses
         add_js_page_action(:title => 'Assign Class', :render => {:partial => 'students/add_to_klass_form', :locals => {:student => @student}})
