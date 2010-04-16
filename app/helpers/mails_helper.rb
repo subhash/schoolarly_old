78737a5a9af24556edae479541a5d3e4a269d75b
parvathy <parvathy@2498623e-5ac1-4a62-989a-52cda5f0dd9e>
2010-04-16 12:24:27 +0000

    list_sender_or_receivers added

--- app/helpers/mails_helper.rb
+++ app/helpers/mails_helper.rb
@@ -4,4 +4,9 @@ module MailsHelper
     return 'No message available'
   end
   
+  def list_sender_or_receivers(mail)
+    users_list = (mail.mailbox == 'inbox') ? [mail.message.sender] : mail.message.recipients
+    users_list.collect {|u| link_to u.person.name, {:controller => u.person_type.pluralize, :action => 'show', :id => u.person}, :title => u.email }.join(', ')
+  end
+  
 end
