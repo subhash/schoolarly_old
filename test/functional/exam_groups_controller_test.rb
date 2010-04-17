15d66e1113e6e1ce981e7087a2a9c766034238ce
parvathy <parvathy@2498623e-5ac1-4a62-989a-52cda5f0dd9e>
2010-04-17 17:57:11 +0000

    test case added

--- test/functional/exam_groups_controller_test.rb
+++ test/functional/exam_groups_controller_test.rb
@@ -21,6 +21,14 @@ class ExamGroupsControllerTest < ActionController::TestCase
     assert_response :success
     assert_template :create_success
   end
+ 
+  test "should fail exam group creation" do   #TODO not so sure how to make it fail
+    assert_difference('ExamGroup.count', 0) do
+      xhr :post, :create, :exam_group => {:exam_type_id => nil, :klass_id => @one_A.to_param}, :exam => {:subject_ids => [@eng.to_param, @mal.to_param]}, :entity_class => 'Klass', :entity_id => @one_A
+    end
+    assert_response :success
+    assert_template :create_failure
+  end
   
   test "should destroy exam_group" do
     assert_difference('ExamGroup.count', -1) do
