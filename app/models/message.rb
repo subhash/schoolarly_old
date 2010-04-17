408cc53dcf45fab0f2201f19d68c154a7659550d
parvathy <parvathy@2498623e-5ac1-4a62-989a-52cda5f0dd9e>
2010-04-17 17:56:04 +0000

    validation added

--- app/models/message.rb
+++ app/models/message.rb
@@ -8,7 +8,7 @@ class Message < ActiveRecord::Base
   belongs_to :sender, :class_name => 'User', :foreign_key => 'sender_id'
   belongs_to :conversation
   has_and_belongs_to_many :recipients, :class_name => 'User', :join_table => 'messages_recipients', :association_foreign_key => 'recipient_id'
-  
+  validates_presence_of :body
   #delivers a message to the the given mailbox of all recipients, calls the on_deliver_callback if initialized.
   #
   #====params:
