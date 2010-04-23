b6156b87835f377adfbd13e2f34aaec3c3d0f001
shenuja <shenuja@2498623e-5ac1-4a62-989a-52cda5f0dd9e>
2010-04-23 05:21:28 +0000

    users method

--- app/models/school.rb
+++ app/models/school.rb
@@ -2,11 +2,13 @@ class School < ActiveRecord::Base
   has_one :user, :as => :person
   has_many :klasses , :order => ["level, division"]
   has_many :teachers 
+  has_many :teacher_users, :through => :teachers, :source => :user
   has_many :students do
     def not_enrolled
       find :all, :conditions => {:klass_id => nil}
     end
   end
+  has_many :student_users, :through => :students, :source => :user
   has_many :exam_groups, :through => :klasses
   has_many :papers, :through => :klasses
   has_many :unallotted_papers, :source => :papers, :through => :klasses, :conditions => ['papers.teacher_id IS NULL']
@@ -21,6 +23,14 @@ class School < ActiveRecord::Base
   
   def exams
     self.exam_groups.collect{|eg| eg.exams}.flatten
+  end
+  
+  def school
+    return self
+  end
+  
+  def users
+    return teacher_users + student_users + user
   end
 
 end
