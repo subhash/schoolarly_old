228fb6205f2c542d474baa77c3f07cce692eabad
parvathy <parvathy@2498623e-5ac1-4a62-989a-52cda5f0dd9e>
2010-04-27 18:07:43 +0000

    mark_and_show is changed to show. create moved to mails from conversations controller

--- app/controllers/mails_controller.rb
+++ app/controllers/mails_controller.rb
@@ -1,4 +1,22 @@
 class MailsController < ApplicationController
+   
+  def new
+    @users = current_user.person.school.users
+  end
+  
+  def create
+    sender=current_user
+    @user= params[:receiver].nil? ? current_user : User.find(params[:receiver])
+    receivers = [User.find(params[:receiver] || params[:mail][:user_ids].compact.reject(&:blank?))].flatten
+    raise if receivers.empty? || params[:message][:body].blank?
+    Mail.transaction do
+        sentMail=sender.send_message(receivers, *params[:message].values)
+    end
+    render :template => 'mails/create_success'
+  rescue Exception => e
+    flash.now[:notice] = e.message + 'Error occurred during posting. Try again...'
+    render :template => 'mails/create_failure'  
+  end
   
   def reply_new
     @mail=Mail.find(params[:id])
@@ -6,29 +24,25 @@ class MailsController < ApplicationController
   
   def send_reply
     @mail=Mail.find(params[:mail])
-    @sentMail=@mail.user.reply_to_sender(@mail, params[:body], params[:subject])
-    sender=@sentMail.user
-    receiver=@mail.message.sender
-    if sender==receiver then @inMail = sender.mailbox[:inbox].latest_mail(:conversation => @sentMail.conversation, :conditions => 'message_id = ' + @sentMail.message.id.to_s).first end
-    render :template => 'conversations/create_success'
+    @user=@mail.user
+    Mail.transaction do
+      @sentMail=@user.reply_to_sender(@mail, params[:body], params[:subject])
+    end
+    render :template => 'mails/create_success'
   rescue Exception => e
+    flash.now[:notice] = 'Error occured during posting. Try again...'
     render :template => 'mails/send_reply_error'
   end
   
-  def mark_and_show
+  def show
     @mail=Mail.find(params[:id])
     @mail.mark_as_read()
-    render :template => 'messages/show'
   end
   
   def destroy
     @mail=Mail.find(params[:id])
-    if !@mail.trashed
-      @mail.user.mailbox.move_to(:trash, :conditions => 'id = ' + @mail.id.to_s)
-    else
-      Mail.delete(@mail)
-    end
-    render :template => 'mails/destroy'
+    @mail.user.mailbox.move_to(:trash, :conditions => 'id = ' + @mail.id.to_s) unless @mail.trashed
+    Mail.delete(@mail) if @mail.trashed
   end
   
 end
