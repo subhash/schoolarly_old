37b44eb175eff70d0a334bdfa58b8ca5e1e612ad
parvathy <parvathy@2498623e-5ac1-4a62-989a-52cda5f0dd9e>
2010-05-03 08:27:42 +0000

    messages review comments incorporated

--- app/controllers/mails_controller.rb
+++ app/controllers/mails_controller.rb
@@ -1,16 +1,12 @@
 class MailsController < ApplicationController
    
-  def new
-    @users = current_user.person.school.users
-  end
-  
   def create
     sender=current_user
-    @user= params[:receiver].nil? ? current_user : User.find(params[:receiver])
-    receivers = [User.find(params[:receiver] || params[:mail][:user_ids].compact.reject(&:blank?))].flatten
-    raise if receivers.empty? || params[:message][:body].blank?
+    @user = User.find(params[:user]) if params[:user]
+    receivers = [User.find(params[:user_ids].compact.reject(&:blank?))].flatten
+    raise if receivers.empty? || params[:body].blank?
     Mail.transaction do
-        sentMail=sender.send_message(receivers, *params[:message].values)
+        sentMail=sender.send_message(receivers, params[:body], params[:subject])
     end
     render :template => 'mails/create_success'
   rescue Exception => e
@@ -24,19 +20,15 @@ class MailsController < ApplicationController
   
   def send_reply
     @mail=Mail.find(params[:mail])
-    @user=@mail.user
+    receivers = [User.find(params[:user_ids].compact.reject(&:blank?))].flatten
+    raise if receivers.empty? || params[:body].blank?
     Mail.transaction do
-      @sentMail=@user.reply_to_sender(@mail, params[:body], params[:subject])
+      @sentMail=@mail.user.reply(@mail.conversation, receivers, params[:body], params[:subject])
     end
     render :template => 'mails/create_success'
   rescue Exception => e
     flash.now[:notice] = 'Error occured during posting. Try again...'
     render :template => 'mails/send_reply_error'
-  end
-  
-  def show
-    @mail=Mail.find(params[:id])
-    @mail.mark_as_read()
   end
   
   def destroy
