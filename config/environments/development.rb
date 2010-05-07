9e6c84165cc5420b5dd4d82399a3f9375011f464
shenuja <shenuja@2498623e-5ac1-4a62-989a-52cda5f0dd9e>
2010-05-07 15:28:32 +0000

    Making actionmailer work

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,7 +18,7 @@ config.action_controller.perform_caching             = false
 config.action_mailer.delivery_method = :smtp
 
 
-config.action_mailer.smtp_settings = {
+ActionMailer::Base.smtp_settings = {
   :tls => "true",
   :address => "smtp.gmail.com",
   :port => "587",
@@ -27,6 +27,6 @@ config.action_mailer.smtp_settings = {
   :user_name => "test@schoolarly.com",
   :password => "testuser"
 }
-config.action_mailer.perform_deliveries = false
+config.action_mailer.perform_deliveries = true
 # Don't care if the mailer can't send
 config.action_mailer.raise_delivery_errors = true
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,21 @@ config.action_view.cache_template_loading            = true
 # config.action_controller.asset_host = "http://assets.example.com"
 
 # Disable delivery errors, bad email addresses will be ignored
-# config.action_mailer.raise_delivery_errors = false
+config.action_mailer.delivery_method = :smtp
+
+
+ActionMailer::Base.smtp_settings = {
+  :tls => "true",
+  :address => "smtp.gmail.com",
+  :port => "587",
+  :domain => "schoolarly.com",
+  :authentication => :plain,
+  :user_name => "test@schoolarly.com",
+  :password => "testuser"
+}
+config.action_mailer.perform_deliveries = true
+# Don't care if the mailer can't send
+config.action_mailer.raise_delivery_errors = false
 
 # Enable threaded mode
 # config.threadsafe!
