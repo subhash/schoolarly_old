441fd97000b925daf6231e8b7467bc6d43beb897
shenuja <shenuja@2498623e-5ac1-4a62-989a-52cda5f0dd9e>
2010-05-11 06:49:02 +0000

    email sending

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -1,5 +1,5 @@
 class Notifier < ActionMailer::Base
-  default_url_options[:host] = "localhost:3004"
+  default_url_options[:host] = "www.schoolarly.com"
   
   def password_reset_instructions(user)
     subject       "Password Reset Instructions"
