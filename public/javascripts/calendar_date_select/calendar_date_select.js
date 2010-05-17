5c8505fed35d66eef5850415b219c9db24db5bc6
subhash <subhash@2498623e-5ac1-4a62-989a-52cda5f0dd9e>
2010-05-17 06:17:23 +0000

    Hack to make calendar_date_select work within Modalbox

--- public/javascripts/calendar_date_select/calendar_date_select.js
+++ public/javascripts/calendar_date_select/calendar_date_select.js
@@ -124,8 +124,11 @@ CalendarDateSelect.prototype = {
       var parent = this.target_element.parentNode;
       var style = {}
     } else {
-      var parent = document.body
-      var style = { position:"absolute", visibility: "hidden", left:0, top:0 }
+//		subhash - Commented out offending lines to make it work within Modalbox
+//      var parent = document.body
+//      var style = { position:"absolute", visibility: "hidden", left:0, top:0 }
+		var parent = this.target_element.parentNode;
+		var style = { position: "fixed", visibility: "hidden", left: 0, top: 0 }
     }
     this.calendar_div = $(parent).build('div', {className: "calendar_date_select"}, style);
     
