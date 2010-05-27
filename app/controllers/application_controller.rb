4cee10da279e8d8b88bc8bfc601f2d8ab1d1a041
parvathy <parvathy@2498623e-5ac1-4a62-989a-52cda5f0dd9e>
2010-05-27 12:21:39 +0000

    exception handlers added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
-  
+
   layout 'schoolarly'
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
@@ -47,13 +47,11 @@ class ApplicationController < ActionController::Base
     @details = options
   end
   
-  
   def render_failure(args)
     render :update do |page|
       page.refresh_dialog args[:refresh]
     end
   end
-  
   
   # :object or :collection
   # :insert or :replace or :delete
@@ -121,38 +119,23 @@ class ApplicationController < ActionController::Base
   end
   
   #Exception Handlers
+
+  rescue_from Exception, :with => :notify_me
+  rescue_from ActiveRecord::RecordNotFound, :with => :show_errors
+  rescue_from ActiveRecord::RecordInvalid, :with => :show_errors
+  rescue_from ActiveRecord::StatementInvalid, :with => :show_errors
+  rescue_from NoMethodError, :with => :show_errors
   
-  #  rescue_from Exception do |exception|
-  #    case  exception
-  #      when ActiveRecord::RecordNotFound 
-  #        then flash_and_redirect_back(exception)
-  #      when ActiveRecord::StatementInvalid 
-  #        then flash_and_redirect_back(exception)
-  #      when ActiveRecord::RecordInvalid 
-  #        then flash_and_redirect_back(exception)
-  #    else
-  #      flash_and_redirect_back(exception)
-  #    end
-  #  end
-  #  
-  #  def flash_and_redirect_back(exception)
-  #    flash[:notice]="Error Occurred: <br /> #{exception.message}"
-  #    redirect_to(session[:parent_url] ? session[:parent_url] : request.request_uri)
-  #  end
+  protected
   
-  #  def get_dialog_content(args)
-  #    collection = args[:collection]
-  #    message = get_message_text(args[:message]) || 'No action allowed'
-  #    render = args[:render]
-  #    return (collection.empty? || render.nil?) ? {:text =>'<blockquote>' + message + '</blockquote>'} : render
-  #  end
-  #  
-  #  def get_message_text(message)
-  #    case message
-  #      when :message_if_empty_unallotted_papers then 'No more papers to add' 
-  #      when :message_if_empty_papers then 'No paper available'
-  #      else message
-  #    end
-  #  end
+  def notify_me
+    Exceptional.rescue do
+      raise
+    end
+  end
+  
+  def show_errors(exception)
+    render :text => exception.message
+  end
   
 end
--- config/environment.rb
+++ config/environment.rb
@@ -23,7 +23,7 @@ Rails::Initializer.run do |config|
   config.gem 'formtastic'
   config.gem 'authlogic'
   config.gem 'calendar_date_select'
-  
+  config.gem 'exceptional'
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
@@ -43,5 +43,4 @@ Rails::Initializer.run do |config|
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   # config.i18n.default_locale = :de
   require 'custom_in_place_editing'
-  
 end
