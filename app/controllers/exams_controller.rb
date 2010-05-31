33e568913d011bfbe09658508e1742ddfed8b5be
shenuja <shenuja@2498623e-5ac1-4a62-989a-52cda5f0dd9e>
2010-05-31 06:49:24 +0000

    Cleaning up exams

--- app/controllers/exams_controller.rb
+++ app/controllers/exams_controller.rb
@@ -10,46 +10,7 @@ class ExamsController < ApplicationController
     @exam=Exam.find(params[:id])
     @exams = [@exam]
   end
-  
-  #  def new
-  #    @exam_group = ExamGroup.find(params[:exam_group])
-  #    @exam = Exam.new(:exam_group => @exam_group)
-  #    @event = Event.new(:start_time => Time.now, :end_time => 1.hour.from_now, :period => "Does not repeat")
-  #    @exam.event = @event   
-  #    entity_class = params[:entity_class]
-  #    @entity = get_entity(entity_class,params[:entity_id])
-  #    #TODO page context
-  #    if entity_class == 'Teacher' || entity_class == 'School'
-  #      @subjects = @entity.subjects_for_klass(@exam_group.klass.id) - @exam_group.subjects
-  #    else
-  #      @subjects = @entity.subjects - @exam_group.subjects      
-  #    end
-  #    #TODO page context
-  #    @teachers = (entity_class == 'School')? @entity.teachers : @entity.school.teachers
-  
-  #  end
-  
-  #  def create
-  #    @exam = Exam.new(params[:exam])
-  #    @exam_group=ExamGroup.find(params[:exam_group_id])
-  #    @exam.exam_group=@exam_group
-  #    @exam.teacher = @exam_group.klass.papers.find_by_subject_id(@exam.subject.id).teacher
-  #    @event = Event.new( :title => @exam.to_s, :description => @exam.to_s, :owner => current_user)
-  #    @event.attributes = params[:event]
-  #    @exam.participants.each do |participant|
-  #      @event.users << participant.user
-  #    end
-  #    @exam.event = @event
-  #    @event.save!
-  #    @entity = get_entity(params[:entity_class],params[:entity_id])
-  #    if @exam.save!
-  #      render :template => 'exams/create_success'
-  #    else
-  #      @subjects = Subject.find(params[:subjects])
-  #      render :template => 'exams/create_failure'
-  #    end
-  #  end
-  
+ 
   def edit
     session[:redirect] = request.request_uri
     @exam = Exam.find(params[:id])
