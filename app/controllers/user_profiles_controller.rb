15b038ebb84b7ba1ad8c19c48a313342a9d12511
parvathy <parvathy@2498623e-5ac1-4a62-989a-52cda5f0dd9e>
2010-06-02 08:08:24 +0000

    change password link added as right side action of profile page for current user

--- app/controllers/user_profiles_controller.rb
+++ app/controllers/user_profiles_controller.rb
@@ -1,10 +1,8 @@
 class UserProfilesController < ApplicationController
   
   before_filter :find_user_and_person
-  before_filter :set_up, :only => [:show, :new, :edit]
-  after_filter :store_parent_url, :only => [:show, :new, :edit]
  
-   def self.in_place_loader_for(object, attribute, options = {})
+  def self.in_place_loader_for(object, attribute, options = {})
     define_method("get_#{object}_#{attribute}") do
       @item = object.to_s.camelize.constantize.find(params[:id])
       render :text => (@item.send(attribute).blank? ? "[No Name]" : @item.send(attribute))
@@ -45,65 +43,18 @@ class UserProfilesController < ApplicationController
     end
   end  
   
-  def set_up
-    set_active_user(@user.id)
-  end
-  
-  def store_parent_url
-    session[:parent_url] = request.request_uri
-  end
-  
-  def new
-    if @user != current_user
-      redirect_to(url_for( :controller => :user_profiles, :action => 'show', :id=>@user))
-    else 
-      if !@user.user_profile.nil?
-        redirect_to(url_for( :controller => :user_profiles, :action => 'edit', :id=>@user))
-      end
-      @user_profile = UserProfile.new
-    end
-  end
-  
-  def create
-    @user_profile = UserProfile.new(params[:user_profile])
-    @user_profile.user=@user
-    User.transaction do
-      @person.update_attributes!(params[:person])
-      @user_profile.save!
-      @user.update_attributes!(params[:user])
-    end
-    flash[:notice] = 'Profile was successfully created.'
-    redirect_to(url_for( :controller => :user_profiles, :action => 'show', :id=>@user))
-  end
-  
   def show
-#    if @user.user_profile.nil? and @user == current_user 
-#      redirect_to(url_for( :controller => :user_profiles, :action => 'new', :id=>@user))
-#    end    
     @user_profile=@user.user_profile
-  end
-  
-  def edit
-#    if @user != current_user
-#      redirect_to(url_for( :controller => :user_profiles, :action => 'show', :id=>@user))
-#    else    
-      if @user.user_profile.nil? 
-        redirect_to(url_for( :controller => :user_profiles, :action => 'new', :id=>@user))
-      else
-        @user_profile=@user.user_profile
-      end
-#    end
-  end
-  
-  def update
-    @user_profile=@user.user_profile
-    User.transaction do
-      @person.update_attributes!(params[:person])
-      @user_profile.update_attributes!(params[:user_profile])
-      @user.update_attributes!(params[:user])
-    end
-    flash[:notice] = 'Profile was successfully updated.'
-    redirect_to(url_for(:controller => :user_profiles, :action => 'show', :id=>@user))
   end
 
+  def update
+    @user.password = params[:user][:password]  
+    @user.password_confirmation = params[:user][:password_confirmation]  
+    if @user.save
+      render :template => 'user_profiles/change_password_success'
+    else
+      render :template => 'user_profiles/change_password_failure'
+    end
+  end
+  
 end
