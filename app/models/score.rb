0ddf1a258cfdd20b546ad750993934d79b289d48
shenuja <shenuja@2498623e-5ac1-4a62-989a-52cda5f0dd9e>
2010-06-07 08:59:01 +0000

    scores , average

--- app/models/score.rb
+++ app/models/score.rb
@@ -1,4 +1,10 @@
 class Score < ActiveRecord::Base
   belongs_to :student
   belongs_to :exam
+  
+  def self.aggregate(scores)
+    averages = scores.group_by{|s|s.exam.activity}.collect{|a, scores| Score.average(:score, :conditions => {:id => scores.collect(&:id)})}
+    (averages.sum.to_f/averages.size).round(2)
+  end
+  
 end
