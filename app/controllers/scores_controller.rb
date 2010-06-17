b01b6db48a62a9a6ee2760afa5cbf7465b01a5f2
shenuja <shenuja@2498623e-5ac1-4a62-989a-52cda5f0dd9e>
2010-06-17 04:05:40 +0000

    cleaning up

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -1,20 +1,4 @@
 class ScoresController < ApplicationController
-#  
-#  def grid_data
-#    @exam = Exam.find(params[:exam])
-#    @students = @exam.students
-#    @students_scores = @students.each_with_object({}) {|student, hash| hash[student] = @exam.scores.find_by_student_id(student.id)}
-#    if(params[:sidx] == 'score')
-#      @students_scores = (params[:sord] == 'asc')? (@students_scores.sort {|a,b| a[1].score<=>b[1].score}) : (@students_scores.sort {|a,b| a[1].score<=>b[1].score}.reverse)
-#    elsif(params[:sidx] == 'name')
-#      @students_scores = (params[:sord] == 'asc')? (@students_scores.sort {|a,b| a[0].name<=>b[0].name}) : (@students_scores.sort {|a,b| a[0].name<=>b[0].name}.reverse)
-#    elsif(params[:sidx] == 'email')
-#      @students_scores = (params[:sord] == 'asc')? (@students_scores.sort {|a,b| a[0].email<=>b[0].email}) : (@students_scores.sort {|a,b| a[0].email<=>b[0].email}.reverse)
-#    end
-#    respond_to do |format|
-#      format.xml {render :partial => 'grid_data.xml.builder', :layout => false }
-#    end   
-#  end
   
   def grid_data
     @exams = Exam.find(params[:exams])
@@ -31,26 +15,6 @@ class ScoresController < ApplicationController
       format.xml {render :partial => 'grid_data.xml.builder', :layout => false }
     end   
   end
-#  
-#  
-#  def row_edit
-#    @exam = Exam.find(params[:exam])
-#    @student = Student.find_by_id(params[:id])
-#    @score =  @exam.scores.find_by_student_id(@student.id)
-#    if(@score)
-#      @score.score = params[:score]
-#    else
-#      @score = Score.new do |s|
-#        s.score = params[:score]
-#        s.student = @student
-#        s.exam = @exam
-#      end
-#    end  
-#    @score.save!
-#    respond_to do |format|
-#      format.js
-#    end  
-#  end
   
   def row_edit
     @student = Student.find_by_id(params[:id])
