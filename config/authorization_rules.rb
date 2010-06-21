722353d5b53aa144d7cf9669cd595a6ac318c3f0
parvathy <parvathy@2498623e-5ac1-4a62-989a-52cda5f0dd9e>
2010-06-21 08:06:53 +0000

    reverting

--- config/authorization_rules.rb
+++ config/authorization_rules.rb
@@ -5,11 +5,7 @@ authorization do
   end
   
   same_school = proc do
-    if_attribute :school => is {user.person.school}, :school => is_not {nil}
-  end
-  
-  same_klass = proc do
-    if_attribute :klass => {:class_teacher => is {user.person}}
+    if_attribute :school => is {user.person.school}
   end
   
   role :guest do
@@ -23,23 +19,13 @@ authorization do
     has_permission_on :scores, :to => :read do
       if_permitted_to :read, :exam
     end
-    has_permission_on :user_profiles, :to => :read_write, &same_user
-    has_permission_on [:schools, :teachers, :students, :klasses], :to => :contact, &same_school
+    has_permission_on :user_profiles, :to => :manage, &same_user
   end
   
   role :teacher do
     includes :student
-    has_permission_on :klasses, :to => :add_remove_students, &same_klass
-    has_permission_on :students, :to => :add_remove_papers, &same_klass
-    has_permission_on :students, :to => :add_to_klass do
-      if_attribute :klass => is {nil}, :school => is {user.person.school}
-    end
     has_permission_on :teachers, :to => :read_write, &same_user
-    has_permission_on :students, :to => :read, &same_school
-    has_permission_on :students, :to => :read_write do
-      if_attribute :klass => is { nil }, :school => is {user.person.school}
-      if_attribute :klass => {:class_teacher => is {user.person}}
-    end
+    has_permission_on :students, :to => :manage, &same_school
     has_permission_on :klasses, :to => :manage, &same_school
     has_permission_on :exams, :to => :manage do
       if_permitted_to :manage, :klass
@@ -50,45 +36,19 @@ authorization do
     has_permission_on :papers, :to => :manage do
       if_permitted_to :manage, :klass
     end
-    has_permission_on :user_profiles, :to => :read_write, &same_user
-    has_permission_on [:schools, :teachers, :students, :klasses], :to => :contact, &same_school
+    has_permission_on :user_profiles, :to => :manage, &same_user
   end
   
   role :school do
     includes :teacher
-    has_permission_on :klasses, :to => [:add_remove_students, :update], &same_school
-    has_permission_on [:klasses, :students, :teachers], :to => :create, &same_school
-    has_permission_on :students, :to => :add_to_klass do
-      if_attribute :klass => is {nil}, :school => is {user.person}
-    end
-    has_permission_on [:klasses, :teachers], :to => :add_remove_papers, &same_school
-    has_permission_on :students, :to => :add_remove_papers do
-      if_attribute :klass => is_not {nil}, :school => is {user.person}
-    end
-    has_permission_on :students, :to => :manage, &same_school
     has_permission_on :teachers, :to => :manage, &same_school
     has_permission_on :schools, :to => :read_write, &same_user
-    has_permission_on :user_profiles, :to => :read_write, &same_user
-    has_permission_on [:schools, :teachers, :students, :klasses], :to => :contact, &same_school
+    has_permission_on :user_profiles, :to => :manage, &same_user
     has_permission_on [:schools, :teachers, :students], :to => :alter, &same_school
   end
   
   role :schoolarly_admin do
-    has_permission_on [:teachers, :students], :to => :add_to_school do
-      if_attribute :school => is {nil}
-    end
-    has_permission_on :students, :to => :add_to_klass do
-      if_attribute :klass => is {nil}
-    end
-    has_permission_on :klasses, :to => :add_remove_papers
-    has_permission_on :teachers, :to => :add_remove_papers do
-      if_attribute :school => is_not {nil}
-    end
-    has_permission_on :students, :to => :add_remove_papers do
-      if_attribute :klass => is_not {nil}
-    end
-    has_permission_on [:users, :schools, :teachers, :students, :klasses, :exams, :scores, :papers, :user_profiles], :to => :manage
-    has_permission_on [:schools, :teachers, :students, :klasses], :to => :contact
+    has_permission_on [:schools, :teachers, :students, :klasses, :exams, :scores, :papers, :user_profiles], :to => :manage
     has_permission_on [:schools, :teachers, :students], :to => :alter
     #has_permission_on :authorization_rules, :to => :manage
   end
@@ -96,7 +56,7 @@ authorization do
 end
 
 privileges do
-  privilege :manage, :includes => [:create, :delete, :read_write]
+  privilege :manage, :includes => [:create, :read, :update, :delete]
   privilege :read_write, :includes => [:read, :update]
   privilege :read, :includes => [:index, :show]
   privilege :create, :includes => [:new, :add]
