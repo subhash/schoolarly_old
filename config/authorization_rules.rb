a21d07f3d049659e8932e10d50cca1bcd759c43c
parvathy <parvathy@2498623e-5ac1-4a62-989a-52cda5f0dd9e>
2010-06-23 20:43:32 +0000

    permissions corrected

--- config/authorization_rules.rb
+++ config/authorization_rules.rb
@@ -5,29 +5,30 @@ authorization do
   end
   
   same_school = proc do
-    if_attribute :school => is {user.person.school}, :school => is_not {nil}
+    if_attribute :school => is {user.person.school} 
+    if_attribute :school => is_not {nil}
   end
   
   role :guest do
   end
   
   role :student do
-    has_permission_on [:schools, :klasses, :teachers, :students], :to => :read, &same_school
+    has_permission_on [:schools, :klasses, :teachers, :students], :to => :read, :join_by => :and, &same_school
     has_permission_on :exams, :to => :read do
       if_attribute :students => contains {user.person}
     end
     has_permission_on :scores, :to => :read do
       if_permitted_to :read, :exam
     end
-    has_permission_on :user_profiles, :to => :manage, &same_user
-    has_permission_on [:schools, :teachers, :students, :klasses], :to => :contact, &same_school
+    has_permission_on :user_profiles, :to => :read_write, &same_user
+    has_permission_on [:schools, :teachers, :students, :klasses, :papers], :to => :contact, :join_by => :and, &same_school
   end
   
   role :teacher do
     includes :student
     has_permission_on :teachers, :to => :read_write, &same_user
-    has_permission_on :students, :to => :manage, &same_school
-    has_permission_on :klasses, :to => :manage, &same_school
+    has_permission_on :students, :to => :manage, :join_by => :and, &same_school
+    has_permission_on :klasses, :to => :manage, :join_by => :and, &same_school
     has_permission_on :exams, :to => :manage do
       if_permitted_to :manage, :klass
     end
@@ -37,28 +38,29 @@ authorization do
     has_permission_on :papers, :to => :manage do
       if_permitted_to :manage, :klass
     end
-    has_permission_on :user_profiles, :to => :manage, &same_user
+    has_permission_on :user_profiles, :to => :read_write, &same_user
   end
   
   role :school do
     includes :teacher
-    has_permission_on :teachers, :to => :manage, &same_school
+    has_permission_on :teachers, :to => :manage, :join_by => :and, &same_school
     has_permission_on :schools, :to => :read_write, &same_user
-    has_permission_on :user_profiles, :to => :manage, &same_user
-    has_permission_on [:schools, :teachers, :students], :to => :alter, &same_school
+    has_permission_on :user_profiles, :to => :read_write, &same_user
+    has_permission_on [:schools, :teachers, :students], :to => :alter, :join_by => :and, &same_school
+    
   end
   
   role :schoolarly_admin do
     has_permission_on [:schools, :teachers, :students, :schoolarly_admins, :klasses, :exams, :scores, :papers, :users, :user_profiles], :to => :manage
     has_permission_on [:schools, :teachers, :students], :to => :alter
-    has_permission_on [:schools, :teachers, :students, :klasses], :to => :contact
+    has_permission_on [:schools, :teachers, :students, :klasses, :papers], :to => :contact
     #has_permission_on :authorization_rules, :to => :manage
   end
   
 end
 
 privileges do
-  privilege :manage, :includes => [:create, :read, :update, :delete]
+  privilege :manage, :includes => [:create, :read_write, :delete]
   privilege :read_write, :includes => [:read, :update]
   privilege :read, :includes => [:index, :show]
   privilege :create, :includes => [:new, :add]
