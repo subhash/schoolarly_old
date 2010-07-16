944eead59a5ca04d1c556a69940d9ace11bd7c22
shenuja <shenuja@2498623e-5ac1-4a62-989a-52cda5f0dd9e>
2010-07-16 04:37:40 +0000

    correcting errors in create

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -4,7 +4,7 @@ class ActivitiesController < ApplicationController
     @assessment = Assessment.find_by_id(params[:assessment_id])
     @assessment_tool = AssessmentTool.new(:assessment => @assessment)
     @activity = Activity.new(:max_score =>@assessment.assessment_type.max_score)
-    @activity.event = Event.new( :start_time => Event.now, :end_time => Event.now.advance(:hours => 1 ))
+    @event = Event.new( :start_time => Event.now, :end_time => Event.now.advance(:hours => 1 ))
     @assessment_tool_names = @assessment.school_subject.assessment_tool_names
     render :update do |page|
       page.open_dialog "New activity for #{@assessment.long_name}", :partial => 'activities/new', :locals => {:activity => @activity}
@@ -16,12 +16,14 @@ class ActivitiesController < ApplicationController
     assessment_tool_existing = @assessment_tool.assessment.assessment_tools.find_by_name(@assessment_tool.name)
     @activity  = Activity.new(params[:activity])
     @activity.assessment_tool = assessment_tool_existing ? assessment_tool_existing : @assessment_tool
-    event_series = EventSeries.new(:title => "#{@assessment_tool.assessment.long_name} : #{@activity.name}", :description => @activity.description, :owner => current_user)
-    @activity.assessment_tool.assessment.participants.each do |participant|
-      event_series.users << participant.user
+    unless(params[:event][:start_time].blank? and params[:event][:end_time].blank?)
+      @event = Event.new(params[:event])
+      event_series = EventSeries.new(:title => "#{@assessment_tool.assessment.long_name} : #{@activity.name}", :description => @activity.description, :owner => current_user)
+      event_series.users = @activity.assessment_tool.assessment.participants.collect(&:user)
+      @event.event_series = event_series
+      event_series.events << @event
+      @activity.event  = @event
     end
-    @activity.event.event_series = event_series
-    event_series.events << @activity.event
     render :update do |page|
       if @activity.save 
         page.close_dialog
