17ae42f6fa1bff8e0c762f18f299aea4fb3b1a82
parvathy <parvathy@2498623e-5ac1-4a62-989a-52cda5f0dd9e>
2010-07-26 06:48:57 +0000

    not required

--- lib/my_helpers.rb
+++
@@ -1,8 +0,0 @@
-module MyHelpers
-  
-  def interval(start_time, end_time)
-    hours, mins, ignore_secs, ignore_fractions = Date::day_fraction_to_time((end_time.to_time - start_time.to_time)/1.day)
-    duration = ActionController::Base.helpers.pluralize(hours, 'hr') + ((mins > 0) ? ' ' + ActionController::Base.helpers.pluralize(mins, 'min') : '')
-  end
- 
-end
