2922ff384634125cf5f4a3ccb08b28677708826d
shenuja <shenuja@2498623e-5ac1-4a62-989a-52cda5f0dd9e>
2010-07-28 04:45:52 +0000

    revoking create to the code before event messaging

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -4,7 +4,7 @@ class ActivitiesController < ApplicationController
     @assessment = Assessment.find_by_id(params[:assessment_id])
     @assessment_tool = AssessmentTool.new(:assessment => @assessment, :weightage => @assessment.assessment_tools.size == 0 ? 100 : 0)
     @activity = Activity.new(:max_score =>@assessment.assessment_type.max_score)
-    @event = Event.new
+    @activity.event = Event.new
     @assessment_tool_names = @assessment.school_subject.assessment_tool_names
     render :update do |page|
       page.open_dialog "New activity for #{@assessment.long_name}", :partial => 'activities/new', :locals => {:activity => @activity}
@@ -16,20 +16,23 @@ class ActivitiesController < ApplicationController
     assessment_tool_existing = @assessment_tool.assessment.assessment_tools.find_by_name(@assessment_tool.name)
     @activity  = Activity.new(params[:activity])
     @activity.assessment_tool = assessment_tool_existing ? assessment_tool_existing : @assessment_tool
-    unless(params[:event][:start_time].blank? and params[:event][:end_time].blank?)
-      @event = Event.new(params[:event])
+    if(@activity.event.start_time.blank? and @activity.event.end_time.blank? )
+      @activity.event = nil
+    else
       event_series = EventSeries.new(:title => "#{@assessment_tool.assessment.long_name} : #{@activity.name}", :description => @activity.description, :owner => current_user)
       event_series.users = @activity.assessment_tool.assessment.current_participants.collect(&:user)
-      @event.event_series = event_series
-      @activity.event  = @event
+      @activity.event.event_series = event_series
     end
-    if @activity.save
-      @assessment = @activity.assessment
-      render :template => 'activities/create_success'
-    else
-      @assessment = @assessment_tool.assessment
-      @assessment_tool_names = @assessment.school_subject.assessment_tool_names
-      render :template => 'activities/create_failure'
+    render :update do |page|
+      if @activity.save
+        @assessment = @activity.assessment
+        page.replace_object @activity.assessment, :partial => 'assessments/assessment'
+        page.open_dialog "Adjust calculations for "+@assessment.long_name, :partial => 'assessments/edit'
+      else
+        @assessment = @assessment_tool.assessment
+        @assessment_tool_names = @assessment.school_subject.assessment_tool_names
+        page.refresh_dialog  :partial => 'activities/new', :locals => {:activity => @activity}
+      end
     end
   end
   
