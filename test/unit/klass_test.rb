3ab6d73168f608938ac2a854e67457b96253be1b
parvathy <parvathy@2498623e-5ac1-4a62-989a-52cda5f0dd9e>
2010-08-16 05:54:21 +0000

    klass_test

--- test/unit/klass_test.rb
+++ test/unit/klass_test.rb
@@ -1,44 +1,47 @@
 require 'test_helper'
 
-
 class KlassTest < ActiveSupport::TestCase
   
   def setup
-    @oneA = klasses :one_A
-    @twoB = klasses :two_B
     @sunil = teachers :sunil
-    @stTheresas = schools :st_teresas
+    @stteresas = schools(:st_teresas)
+    @level = levels(:three)
+    @klass_with_students = klasses(:klass_with_students)
+    @klass_with_papers = klasses(:klass_with_papers)
+    @student = students(:student1_for_klass_with_students)
   end
   
   test "klass-crud" do
-    three_c = Klass.new(:level => levels(:three), :division =>'C')
-    assert_difference('@stTheresas.klasses.size') do 
-      three_c.school = @stTheresas
+    three_c = Klass.new(:level => @level, :division => 'C')
+    assert_difference('@stteresas.klasses.size', 1) do 
+      three_c.school = @stteresas
       three_c.save!
     end
-    assert_difference('@stTheresas.klasses.size', -1) do
+    assert_difference('@stteresas.klasses.size', -1) do
       three_c.destroy
     end
   end
   
-  test "klass-can-be-destroyed" do
-    three_c = Klass.new(:level => levels(:three), :division =>'C')
-    three_c.school = @stTheresas
+  test "klass can be destroyed" do
+    three_c = Klass.new(:level => @level, :division =>'C')
+    three_c.school = @stteresas
     three_c.save!
     assert three_c.can_be_destroyed
-    assert !@oneA.can_be_destroyed
+    assert !@klass_with_students.can_be_destroyed
+    assert !@klass_with_papers.can_be_destroyed
   end
   
   test "klass-school-teacher relationship" do
-    assert_equal @oneA.school, @stTheresas
-    assert_equal @oneA.class_teacher, @sunil
+    three_c = Klass.create(:level => @level, :division =>'C', :school => @stteresas, :teacher_id => @sunil.id)
+    assert_equal three_c.school, @stteresas
+    assert @stteresas.klasses.include?(three_c)
+    assert_equal three_c.class_teacher, @sunil
+    assert @sunil.owned_klasses.include?(three_c)
   end
   
   test "klass-student relationship" do
-    assert_equal 2, @oneA.students.size    
-    shenu = students(:shenu)    
-    assert_equal @oneA, shenu.klass    
+    assert_equal 2, @klass_with_students.students.size    
+    assert_equal @klass_with_students, @student.klass    
   end
-  
   
 end
