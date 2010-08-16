1a06e02585f2fac1874068762b58951a14b6b631
parvathy <parvathy@2498623e-5ac1-4a62-989a-52cda5f0dd9e>
2010-08-16 03:31:06 +0000

    unit test cases for academic year added

--- test/unit/academic_year_test.rb
+++ test/unit/academic_year_test.rb
@@ -3,9 +3,11 @@ require 'test_helper'
 class AcademicYearTest < ActiveSupport::TestCase
 
   def setup
-    @sboa = schools(:sboa)
+    @sboa_student = students(:sboa_student)
     @stteresas = schools(:st_teresas)
     @stteresas_year = academic_years(:st_teresas_year)
+    @oneA = klasses(:one_A)
+    @marykkutty = teachers(:marykkutty)
   end
   
   test "school can have many academic years" do
@@ -15,5 +17,17 @@ class AcademicYearTest < ActiveSupport::TestCase
   test "current academic year is the one with the latest start_date for the school" do
     assert_equal @stteresas.academic_year, @stteresas_year
   end
+ 
+  test "academic year of a klass is its schools current academic year" do
+    assert_equal @oneA.academic_year, @oneA.school.academic_year
+  end
   
+  test "a student can have an academic year which is his schools current academic year" do
+    assert_equal @sboa_student.academic_year, @sboa_student.school.academic_year
+  end
+
+  test "a teacher can have an academic year which is his schools current academic year" do
+    assert_equal @marykkutty.academic_year, @marykkutty.school.academic_year
+  end
+
 end
