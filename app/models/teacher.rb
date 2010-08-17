40efcf07af2c92daae5b15a3712539d09fa14496
parvathy <parvathy@2498623e-5ac1-4a62-989a-52cda5f0dd9e>
2010-08-17 06:11:41 +0000

    removed for_subject

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -2,11 +2,12 @@ class Teacher < ActiveRecord::Base
   has_one :user, :as => :person
   belongs_to :school
   has_many :papers
-  has_many :klasses, :through => :papers, :uniq => true do
-    def for_subject(subject_id)
-      find :all , :conditions => ['papers.subject_id = ?', subject_id]
-    end
-  end
+  has_many :klasses, :through => :papers, :uniq => true 
+#  do
+#    def for_subject(subject_id)
+#      find :all , :conditions => ['papers.subject_id = ?', subject_id]
+#    end
+#  end
   has_many :subjects, :through => :papers, :uniq => true, :order => 'name'
   has_many :owned_klasses, :class_name => 'Klass'
   has_one :academic_year, :through => :school, :order => "start_date DESC"
