c30562a6d7be6ec465293118e99987ae85208f9d
parvathy <parvathy@2498623e-5ac1-4a62-989a-52cda5f0dd9e>
2010-08-17 04:07:58 +0000

    user_profile tests added

--- test/unit/user_profile_test.rb
+++ test/unit/user_profile_test.rb
@@ -4,23 +4,27 @@ class UserProfileTest < ActiveSupport::TestCase
   
   def setup
     @paru = users(:paru)
-    @stTeresas = user_profiles(:stTeresas)
+    @stteresas = user_profiles(:stTeresas)
+  end 
+  
+  test "profile belongs to user" do
+    assert_nil @paru.user_profile
+    assert_difference('UserProfile.count', 1) do
+      paru_profile = UserProfile.create(:name => 'Paru', :user => @paru)
+      @paru.user_profile = paru_profile
+      paru_profile.user = @paru
+    end
   end
   
-  test "profile-create" do
-    assert_nil @paru.user_profile
-    paru_profile = UserProfile.new(:name =>'Name', :user => @paru)
-    assert paru_profile.valid?
-    paru_profile.save!
-    assert paru_profile.valid?
-    assert_equal 'Name', paru_profile.name.to_s
+  test "validate presence of name" do
+    profile_valid = UserProfile.new(:name => 'name', :user => @user)
+    profile_invalid = UserProfile.new(:user => @user)
+    assert profile_valid.valid?
+    assert !profile_invalid.valid?
   end
   
   test "address should give the entire address of the profile seperated by comma" do
-    assert_equal 'St Teresas Convent, Convent Junction, Kochi, Kerala, India', @stTeresas.address.to_s
+    assert_equal 'St Teresas Convent, Convent Junction, Kochi, Kerala, India', @stteresas.address.to_s
   end
-  
-  test "name should give the entire name of the user seperated by space" do
-    assert_equal 'St. Teresas', @stTeresas.name.to_s
-  end
+
 end
